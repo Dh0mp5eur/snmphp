ac9908eaba110f53abfe514d4014818a27fe3f5f
Lorenzo Cafaro <lorenzo@interstella.net>
2015-06-30 00:21:21 +0200

    added system info for synology

--- demo.php
+++ demo.php
@@ -4,7 +4,9 @@
   include("synology.php");
 
   $rs815 = new synology("192.168.10.15", "public");
+
   print_r($rs815->get_disk_info());
   print_r($rs815->get_system_info());
   //print_r($rs815->get_network_info())
+
 ?>
