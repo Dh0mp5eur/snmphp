7e6e398166113db7c62e839ab0b741057e5a2e90
Lorenzo Cafaro <lorenzo@interstella.net>
2015-06-30 10:10:05 +0200

    procurve 1920-48g improvements

--- procurve.php
+++ procurve.php
@@ -11,9 +11,20 @@
       $this->snmp = new snmp_querier($ip, $community);
 		}
 
-    function get_iface_names() {
-      $iface_names = $this->snmp->walk("IF-MIB::ifName");
-      return $iface_names;
+    function get_iface_status_short() {
+      $iface_list = $this->snmp->map_oids($this->snmp->walk("IF-MIB::ifDescr"));
+      foreach($iface_list as $iface_name=>$iface_oid) {
+        $iface_status_short[$iface_name] = $this->snmp->get("IF-MIB::ifOperStatus.$iface_oid");
+      }
+      return $iface_status_short;
+    }
+
+    function get_iface_status_detail() {
+      $iface_list = $this->snmp->map_oids($this->snmp->walk("IF-MIB::ifDescr"));
+      foreach($iface_list as $iface_name=>$iface_oid) {
+        $iface_status_detail[$iface_name]['Link:'] = $this->snmp->get("IF-MIB::ifOperStatus.$iface_oid");
+      }
+      return $iface_status_detail;
     }
 	}
 
